82341946370f7dd61d0e3796115675052b54b04f
Lee Brandt <leebrandt@gmail.com>
2013-01-28 18:41:41 -0600

    Initial Commit

---
+++ kcdcios/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  kcdcios
+//
+//  Created by Lee Brandt on 1/28/13.
+//  Copyright (c) 2013 Lee Brandt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ kcdcios/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  kcdcios
+//
+//  Created by Lee Brandt on 1/28/13.
+//  Copyright (c) 2013 Lee Brandt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
